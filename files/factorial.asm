a32e2f48efe78c88d9fd982b749a323fadd56bfb
Ruixue Li <lrxpnl@gmail.com>
2014-04-12 12:40:52 -0400

    clean up

---
+++ files/factorial.asm
@@ -0,0 +1,34 @@
+fact:
+ 
+	slti $t0, $a0, 2	# if i < 2 (i.e i == 1)
+	beq $t0, $zero, cont	# if i >= 2 go to cont
+	addi $v0, $zero, 1	# else make the resturn value 1
+ 
+	jr $ra
+ 
+ 
+cont:
+ 
+	# OPERATION 1: save into stack
+ 
+	addi $sp, $sp, -8	# make space in the stack
+	sw $ra, 0($sp)		# save the return address
+	sw $a0, 4($sp)		# save the argument value
+ 
+	# OPERATION 2: compute fact(n - 1)
+ 
+	addi $a0, $a0, -1
+	jal fact
+ 
+	# OPERATION 3: restore from stack
+ 
+	lw $ra, 0($sp)		# get old return address from stack
+	lw $a0, 4($sp)		# get old argument value from stack
+	addi $sp, $sp, 8	# return stack pointer to original value, thus erasing all values
+ 
+	# OPERATION 4: finally n * fact(n - 1)
+ 
+	mult $v0, $a0		# multiply n * fib(n - 1)
+	mflo $v0		# gets the result of the multiplication from the low register
+ 
+	jr $ra
--- src/edu/nyu/vmproj/assemble/MyVM.java
+++ src/edu/nyu/vmproj/assemble/MyVM.java
@@ -8,65 +8,62 @@ import java.io.PrintWriter;
 
 public class MyVM {
   // map: instr addr -> instr
-	Program prog;
-	// map: reg name -> value
-	RegisterMap regMap;
-	// map: label name -> instr addr
-	LabelMap labelMap;
-	// mem addr -> value
-	Memory memory;
-	
-	OperandFactory oprFact;
-	// profile: dbbMap(dynamic basic block map): dbb -> executing times
-	Profile profile;
-	int curDBBStart;
-	int curDBBEnd;
-	//TODO: for test
-	static PrintWriter out;
-	
-	private void setEIP(Integer i) {
-		regMap.put("eip", new Integer(i));
-	}
-	
-	private void incEIP() {
-		regMap.put("eip", new Integer(regMap.get("eip") + 4));
-	}
-	
-	private void setFLAGS(int v) {
-		regMap.put("flags", new Integer(v));
-	}
-	
-	private int getFLAGS() {
-		return regMap.get("flags");
-	}
-	
-	private void setREMAINDER(int v) {
-	  regMap.put("remainder", new Integer(v));
-	}
-	
-	private int getREMAINDER() {
-	  return regMap.get("remainder");
-	}
-	
-	private void execute(Instruction inst) {
-		String op = inst.getOp();
-		// I. Memory
-		if (op.equals("mov")) {
-			Readable rop2 = oprFact.buildRValue(inst.getArg2());
-			Assignable wop1 = oprFact.buildLValue(inst.getArg1());
-			wop1.assign(rop2.read());
-			incEIP();
-			return;
-		}
-		
-		// II. Stack
-		if (op.equals("push")) {
-		  Readable rop1 = oprFact.buildRValue(inst.getArg1());
-		  memory.push(rop1.read());
-		  incEIP();
-		  return;
-		}
-		
+  Program prog;
+  // map: reg name -> value
+  RegisterMap regMap;
+  // map: label name -> instr addr
+  LabelMap labelMap;
+  // mem addr -> value
+  Memory memory;
+  
+  OperandFactory oprFact;
+  
+  //TODO: for test
+  PrintWriter out;
+  
+  private void setEIP(Integer i) {
+    regMap.put("eip", new Integer(i));
+  }
+  
+  private void incEIP() {
+    regMap.put("eip", new Integer(regMap.get("eip") + 4));
+  }
+  
+  private void setFLAGS(int v) {
+    regMap.put("flags", new Integer(v));
+  }
+  
+  private int getFLAGS() {
+    return regMap.get("flags");
+  }
+  
+  private void setREMAINDER(int v) {
+    regMap.put("remainder", new Integer(v));
+  }
+  
+  private int getREMAINDER() {
+    return regMap.get("remainder");
+  }
+  
+  private void execute(Instruction inst) {
+    String op = inst.getOp();
+    // I. Memory
+    if (op.equals("mov")) {
+      Readable rop2 = oprFact.buildRValue(inst.getArg2());
+      Assignable wop1 = oprFact.buildLValue(inst.getArg1());
+      wop1.assign(rop2.read());
+      incEIP();
+      return;
+    }
+    
+    // II. Stack
+    if (op.equals("push")) {
+      Readable rop1 = oprFact.buildRValue(inst.getArg1());
+      memory.push(rop1.read());
+      incEIP();
+      return;
+    }
+    
     if (op.equals("pop")) {
       Assignable wop1 = oprFact.buildLValue(inst.getArg1());
       wop1.assign(memory.pop());
@@ -84,60 +81,54 @@ public class MyVM {
       return;
     }
     
-		// III. Calling Conventions
+    // III. Calling Conventions
     if (op.equals("call")) {      
       memory.push(regMap.get("eip"));
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       setEIP(labelMap.get(inst.getArg1()));
-      curDBBStart = regMap.get("eip");
       return;
     }
     
     if (op.equals("ret")) {
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       setEIP(memory.pop());
       incEIP();
-      curDBBStart = regMap.get("eip");
       return;
     }
     
-		// IV. Arithmetic Operations
-		if (op.equals("inc")) {
-		  Readable rop1 = oprFact.buildRValue(inst.getArg1());
-		  Assignable wop1 = oprFact.buildLValue(inst.getArg1());
-		  wop1.assign(rop1.read() + 1);
-		  incEIP();
-		  return;
-		}
-		
-		if (op.equals("dec")) {
-		  Readable rop1 = oprFact.buildRValue(inst.getArg1());
+    // IV. Arithmetic Operations
+    if (op.equals("inc")) {
+      Readable rop1 = oprFact.buildRValue(inst.getArg1());
+      Assignable wop1 = oprFact.buildLValue(inst.getArg1());
+      wop1.assign(rop1.read() + 1);
+      incEIP();
+      return;
+    }
+    
+    if (op.equals("dec")) {
+      Readable rop1 = oprFact.buildRValue(inst.getArg1());
       Assignable wop1 = oprFact.buildLValue(inst.getArg1());
       wop1.assign(rop1.read() - 1);
       incEIP();
       return;
     }
-		
-		if (op.equals("add")) {
-			Readable rop2 = oprFact.buildRValue(inst.getArg2());
-			Readable rop1 = oprFact.buildRValue(inst.getArg1());
-			Assignable wop1 = oprFact.buildLValue(inst.getArg1());
-			wop1.assign(rop1.read() + rop2.read());
-			incEIP();
-			return;
-		}
     
-		if (op.equals("sub")) {
-		  Readable rop2 = oprFact.buildRValue(inst.getArg2());
+    if (op.equals("add")) {
+      Readable rop2 = oprFact.buildRValue(inst.getArg2());
+      Readable rop1 = oprFact.buildRValue(inst.getArg1());
+      Assignable wop1 = oprFact.buildLValue(inst.getArg1());
+      wop1.assign(rop1.read() + rop2.read());
+      incEIP();
+      return;
+    }
+    
+    if (op.equals("sub")) {
+      Readable rop2 = oprFact.buildRValue(inst.getArg2());
       Readable rop1 = oprFact.buildRValue(inst.getArg1());
       Assignable wop1 = oprFact.buildLValue(inst.getArg1());
       wop1.assign(rop1.read() - rop2.read());
       incEIP();
       return;
     }
-		
+    
     if (op.equals("mul")) {
       Readable rop2 = oprFact.buildRValue(inst.getArg2());
       Readable rop1 = oprFact.buildRValue(inst.getArg1());
@@ -171,7 +162,7 @@ public class MyVM {
       return;
     }
     
-		// V. Binary Operators
+    // V. Binary Operators
     if (op.equals("not")) {
       Assignable wop1 = oprFact.buildLValue(inst.getArg1());
       Readable rop1 = oprFact.buildRValue(inst.getArg1());
@@ -225,91 +216,70 @@ public class MyVM {
       return;
     }
     
-		// VI. Comparison
-		if (op.equals("cmp")) {
-			Readable rop2 = oprFact.buildRValue(inst.getArg2());
-			Readable rop1 = oprFact.buildRValue(inst.getArg1());
-			setFLAGS(rop1.read() - rop2.read());
-			incEIP();
-			return;
-		}
-		
-		// VII. Control Flow Manipulation
+    // VI. Comparison
+    if (op.equals("cmp")) {
+      Readable rop2 = oprFact.buildRValue(inst.getArg2());
+      Readable rop1 = oprFact.buildRValue(inst.getArg1());
+      setFLAGS(rop1.read() - rop2.read());
+      incEIP();
+      return;
+    }
+    
+    // VII. Control Flow Manipulation
     if (op.equals("jmp")) {
-      String label = inst.getArg1();
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
+      String label = inst.getArg1();      
       setEIP(labelMap.get(label));
-      curDBBStart = regMap.get("eip");
       return;
     }
     
     if (op.equals("je")) {
       String label = inst.getArg1();
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       if (getFLAGS() == 0) {
         setEIP(labelMap.get(label));
       } else incEIP();
-      curDBBStart = regMap.get("eip");
       return;
     }
-		
+    
     if (op.equals("jne")) {
       String label = inst.getArg1();
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       if (getFLAGS() != 0) {
         setEIP(labelMap.get(label));
       } else incEIP();
-      curDBBStart = regMap.get("eip");
       return;
     }
     
     if (op.equals("jg")) {
       String label = inst.getArg1();
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       if (getFLAGS() > 0) {
         setEIP(labelMap.get(label));
       } else incEIP();
-      curDBBStart = regMap.get("eip");
       return;
     }
     
     if (op.equals("jge")) {
       String label = inst.getArg1();
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       if (getFLAGS() >= 0) {
         setEIP(labelMap.get(label));
       } else incEIP();
-      curDBBStart = regMap.get("eip");
       return;
     }
     
-		if (op.equals("jl")) {
-			String label = inst.getArg1();
-			curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
-			if (getFLAGS() < 0) {
-				setEIP(labelMap.get(label));
-			} else incEIP();
-			curDBBStart = regMap.get("eip");
-			return;
-		}
-			
+    if (op.equals("jl")) {
+      String label = inst.getArg1();
+      if (getFLAGS() < 0) {
+        setEIP(labelMap.get(label));
+      } else incEIP();
+      return;
+    }
+      
     if (op.equals("jle")) {
       String label = inst.getArg1();
-      curDBBEnd = regMap.get("eip");
-      profile.updateDBBMap(new DBB(curDBBStart,curDBBEnd));
       if (getFLAGS() <= 0) {
         setEIP(labelMap.get(label));
       } else incEIP();
-      curDBBStart = regMap.get("eip");
       return;
     }
- 		
+    
     // VIII. Input/Output
     if (op.equals("prn")) {
       Readable rop1 = oprFact.buildRValue(inst.getArg1());
@@ -319,52 +289,41 @@ public class MyVM {
       incEIP();
       return;
     }
-	}
-	
-	public void run(String fileName) {
-		Program.initialize(fileName);
-		regMap = RegisterMap.getInstance();
-		prog = Program.getInstance();
-		labelMap = LabelMap.getInstance();
-		if (labelMap.contains("start")) {
-		  regMap.put("eip", labelMap.get("start"));				  
-		}else {
-		  regMap.put("eip", new Integer(0));		  
-		}
-		curDBBStart = regMap.get("eip");
-		regMap.put("esp", new Integer(10000000));
-		regMap.put("ebp", new Integer(10000000));
-		memory = Memory.getInstance();
-		oprFact = OperandFactory.getInstance();
-		profile = Profile.getInstance();
-		Instruction inst = prog.getInst(regMap.get("eip"));
-		while (inst != null) {
-			execute(inst);
-			inst = prog.getInst(regMap.get("eip"));
-		}
-		// print out profile statistics
-		curDBBEnd = regMap.get("eip")-4;
-		if (curDBBEnd >= curDBBStart) {
-		  DBB dbb = new DBB(curDBBStart,curDBBEnd);
-		  if (!profile.contains(dbb)) {
-		    profile.updateDBBMap(dbb);
-		  }		  
-		}   
-		profile.printDBBMap();
-	}
-	
-	public static void main(String[] args) throws IOException {
-		if (args.length < 1) {
-			System.out.println("Please input the filename: MyVM <filename>");
-			System.exit(0);
-		} else {
-			String fileName = args[0];
-			System.out.println("Running "+fileName+".");
-			MyVM vm = new MyVM();
-			//TODO: for test
-			MyVM.out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".out")));
-			vm.run(fileName);
-			MyVM.out.close();
-		}
-	}
+  }
+  
+  public void run(String fileName) {
+    Program.initialize(fileName);
+    regMap = RegisterMap.getInstance();
+    prog = Program.getInstance();
+    labelMap = LabelMap.getInstance();
+    if (labelMap.contains("start")) {
+      regMap.put("eip", labelMap.get("start"));         
+    }else {
+      regMap.put("eip", new Integer(0));      
+    }
+    regMap.put("esp", new Integer(10000000));
+    regMap.put("ebp", new Integer(10000000));
+    memory = Memory.getInstance();
+    oprFact = OperandFactory.getInstance();
+    Instruction inst = prog.getInst(regMap.get("eip"));
+    while (inst != null) {
+      execute(inst);
+      inst = prog.getInst(regMap.get("eip"));
+    }
+  }
+  
+  public static void main(String[] args) throws IOException {
+    if (args.length < 1) {
+      System.out.println("Please input the filename: MyVM <filename>");
+      System.exit(0);
+    } else {
+      String fileName = args[0];
+      //System.out.println("Running "+fileName+".");
+      MyVM vm = new MyVM();
+      //TODO: for test
+      //vm.out = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".out")));
+      vm.run(fileName);
+      //vm.out.close();
+    }
+  }
 }
